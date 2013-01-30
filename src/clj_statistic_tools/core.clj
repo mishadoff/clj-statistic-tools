022350092ad8b40ad9043af46d367a608824f406
mishadoff <mishadoff@gmail.com>
2013-01-31 00:47:46 +0200

    Add project directories

---
+++ src/clj_statistic_tools/core.clj
@@ -0,0 +1,6 @@
+(ns clj-statistic-tools.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj_statistic_tools/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-statistic-tools.core-test
+  (:use clojure.test
+        clj-statistic-tools.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
